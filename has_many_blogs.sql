d05c24546e105f26aadb0e2d0d957d45027bfd90
Alexis <alexisko@hawaii.edu>
2017-07-26 21:46:20 -1000

    Created users table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,3 +1,17 @@
 CREATE USER has_many_user;
 
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL,
+  username VARCHAR(90) NOT NULL,
+  first_name VARCHAR(90) DEFAULT NULL,
+  last_name VARCHAR(90) DEFAULT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
+);
+
+
+
