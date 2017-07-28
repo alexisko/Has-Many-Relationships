78b75c94cce9b6f85c78c72042bd4e82d74dd988
Alexis <alexisko@hawaii.edu>
2017-07-27 18:41:25 -1000

    Fixed tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -5,7 +5,7 @@ CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(90) NOT NULL,
   first_name VARCHAR(90) DEFAULT NULL,
   last_name VARCHAR(90) DEFAULT NULL,
@@ -16,25 +16,22 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(180) DEFAULT NULL,
   url VARCHAR(510) DEFAULT NULL,
   content TEXT DEFAULT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  user_id INTEGER NOT NULL REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   body VARCHAR(510) DEFAULT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  post_id INTEGER NULL NULL REFERENCES posts(id)
 );
-
-ALTER TABLE users ADD PRIMARY KEY(id);
-
-ALTER TABLE posts ADD PRIMARY KEY(id);
-
-ALTER TABLE comments ADD PRIMARY KEY(id);
